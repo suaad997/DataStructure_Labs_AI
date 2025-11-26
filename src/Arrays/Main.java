8bf62f12141ffd8a3bb7e3d7f2c41eae4e5b5a91
suada <suadaljuaid1@gmail.com>
2025-11-26 19:38:37 +0300

    lab3-Arrays-part2

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -5,18 +5,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
-Arrayss arr= new Arrayss();
-        int []numbers2=new int[5];
+       Arrayss arr= new Arrayss();
+       int []numbers={1,2,3,4,5};
+        arr.LinearSearch(numbers,5);
 
 
-        System.out.println("enter "+numbers2.length+" numbers:");
-        for (int i =0; i<numbers2.length;i++)
-        numbers2[i]=scanner.nextInt();
 
-       // int []numbers={1,2,3,4,5,6,7};
-        arr.Traversal(numbers2);
-       // arr.R_Traversal(numbers);
+        // int []numbers2=new int[5];
+        //numbers[1]=8;
+        // arr.input(numbers2);
+        // arr.update(numbers,8,7);
+        //arr.delete(numbers,3); //numbers[1]=8;
+        //       // arr.input(numbers2);
+        //       // arr.update(numbers,8,7);
+        //        //arr.delete(numbers,3);       // arr.shiftLeft_Delete(numbers,3);
+
+       // arr.Traversal(numbers);
+
+
+        // arr.R_Traversal(numbers);
 
 
         //System.out.println(Arrays.toString(numbers2));
