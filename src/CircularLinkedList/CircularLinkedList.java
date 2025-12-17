7ad8c44c38fbcabd6ffa898e42cda3bf209f81e4
suada <suadaljuaid1@gmail.com>
2025-12-17 13:55:18 +0300

    lab7- CircularLinkedList-pt2+ DoublyLinkedList Pt 0.5

--- src/CircularLinkedList/CircularLinkedList.java
+++ src/CircularLinkedList/CircularLinkedList.java
@@ -35,8 +35,62 @@ size++;
 
  }
 
+public E getFirst(){
+    return tail.getNext().getData();
+ }
+public E getLast(){
+        return tail.getData();
+    }
+public E removeFirst(){
+    if (isEmpty())return null;
+    E delete=tail.getNext().getData();
+    if(tail==tail.getNext()){
+        tail=null;
+    }
+    else {
+        tail.setNext(tail.getNext().getNext());
+    }
+    size--;
+    return delete;
+}
+public E removeLast(){
+    if (isEmpty())return null;
+    E delete=tail.getData();
+    if(tail==tail.getNext()){
+        tail=null;
+    }
+    else {
+        Node<E>temp=tail.getNext();
+        while (temp.getNext()!=tail){
+            temp=temp.getNext();
+        }temp.setNext(tail.getNext());
+        tail=temp;
+    }
+    size--;
+    return delete;
+}
+public void display(){
+Node<E>temp=tail.getNext();
+do{
+    System.out.print(temp.getData()+"--->");
+    temp=temp.getNext();
+    }while (temp!=tail.getNext());
+    System.out.println("Go first("+temp.getData()+")");
+}
 
-    class Node<E> {
+
+
+
+
+
+
+
+
+
+
+
+
+class Node<E> {
 private E data;
 private Node<E> next;
 
---
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,45 @@
+package DoublyLinkedList;
+
+public class DoublyLinkedList {
+
+
+
+
+
+class Node<E>{
+    private Node<E> prev;
+    private E data;
+    private Node<E> next;
+
+    public Node(Node<E> prev, E data, Node<E> next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<E> prev) {
+        this.prev = prev;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+}
+
+}
